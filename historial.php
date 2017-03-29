8a24783e777b7949c5d44e5c889ecbe7162f37f4
Manuel <jcano@ucol.mx>
2017-03-29 14:23:30 -0600

    Se agrego la parte del historial como historial.php

---
+++ historial.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="shortcut icon" type="image/x-ico" href="imagenes/gif.ico">
+	<meta name="tipo_contenido"  content="text/html;" http-equiv="content-type" charset="utf-8">
+	<meta name="p:domain_verify" content="c07ded2dffc377dafa711a2cab31fd54"/>
+
+	<title>Soluciones en Tecnología</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css"/>
+    <link rel="stylesheet" type="text/css" href="css/estilos-movil.css"/>
+      <?php 
+      include("D_Movil.php");
+      if($ismobile) 
+      	{ 
+      ?>
+      <link rel="stylesheet" type="text/css" href="css/estilos-movil.css"/>
+      		<?php
+        }
+      ?>
+      <link rel="stylesheet" type="text/css" href="css/style.css"/>      
+	  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<?php
+  	include("barra.html");
+	?>
+	<br>
+	<br>
+	<br>
+	<br>
+	<?php 
+     include("footer.html");
+	?>
+	
+</body>
+</html>
